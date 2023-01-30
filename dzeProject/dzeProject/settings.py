6ef2f0de80b09d724026f5aa73c455a156b5aea1
Shau Tsar <shautsar@mail.ru>
2023-01-30 20:23:06 +0500

    update 1.0.8

--- dzeProject/dzeProject/settings.py
+++ dzeProject/dzeProject/settings.py
@@ -33,7 +33,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dzeWeb@gmail.com'
-EMAIL_HOST_PASSWORD = 'qpixmhurgdsafzte'
+EMAIL_HOST_PASSWORD = 'fpyjrbwucppephqh'
 
 
 
